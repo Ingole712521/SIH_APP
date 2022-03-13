40e4f8220a0d79181e2febd9d205d705035faaef
Nehal Ingole <nehal712521@gmail.com>
2022-03-13 11:05:42 +0530

    main change

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,9 @@ class  HomePage extends StatelessWidget {
             UserAccountsDrawerHeader(
               accountName: Text("Nehal Ingole"),
               accountEmail: Text("SIH@gmail.com"),
-              currentAccountPicture: Image.network("https://previews.123rf.com/images/yupiramos/yupiramos1607/yupiramos160710209/60039275-%E8%8B%A5%E3%81%84%E7%94%B7%E6%80%A7%E6%BC%AB%E7%94%BB%E3%83%97%E3%83%AD%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%80%81%E3%83%99%E3%82%AF%E3%83%88%E3%83%AB-%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88-%E3%82%B0%E3%83%A9%E3%83%95%E3%82%A3%E3%83%83%E3%82%AF-%E3%83%87%E3%82%B6%E3%82%A4%E3%83%B3%E3%80%82.jpg"),
+              currentAccountPicture: CircleAvatar(
+                child: Image.network("https://previews.123rf.com/images/yupiramos/yupiramos1607/yupiramos160710209/60039275-%E8%8B%A5%E3%81%84%E7%94%B7%E6%80%A7%E6%BC%AB%E7%94%BB%E3%83%97%E3%83%AD%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%80%81%E3%83%99%E3%82%AF%E3%83%88%E3%83%AB-%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88-%E3%82%B0%E3%83%A9%E3%83%95%E3%82%A3%E3%83%83%E3%82%AF-%E3%83%87%E3%82%B6%E3%82%A4%E3%83%B3%E3%80%82.jpg")
+                ),
               
               ),
 
